373549a94ccd6478b03b9f8c8122b08c2616d449
Man7657 <boyamanojkumar98@gmail.com>
2022-07-14 11:46:26 +0530

    Practice file

--- sample.py
+++ sample.py
@@ -5,12 +5,5 @@
 # t = tuple(l)
 # print(t)
 
-# import datetime
-# time1 = datetime.datetime(2020,12,27,13,45,23)
-# time2 = datetime.datetime(2020,12,29,13,50,23)
-#
-# delta = time2-time1
-# print(delta)
 
-print(complex(5))
 
